6e8c2fd3093233469f8370af75cbd8b711f48d75
Grácouš <gracous1234@gmail.com>
2024-06-22 16:16:41 +0200

    Wigu files

---
+++ EnemyReleveler/Settings.cs
@@ -0,0 +1,9 @@
+namespace EnemyReleveler
+{
+    public class Settings
+    {
+        public bool UseZEditConfiguration { get; set; } = true;
+        public int GlobalOffset { get; set; } = 0;
+        public bool PrintDebugOutput { get; set; } = false;
+    }
+}
