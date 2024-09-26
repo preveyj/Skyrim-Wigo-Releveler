d66d0913bfcdd08b0cbaf19408ea7893b265cf67
preveyj <preveyj@gmail.com>
2024-09-25 20:22:16 -0600

    Adding the rest of the modifications

--- EnemyReleveler/Settings.cs
+++
@@ -1,9 +0,0 @@
-namespace EnemyReleveler
-{
-    public class Settings
-    {
-        public bool UseZEditConfiguration { get; set; } = true;
-        public int GlobalOffset { get; set; } = 0;
-        public bool PrintDebugOutput { get; set; } = false;
-    }
-}
